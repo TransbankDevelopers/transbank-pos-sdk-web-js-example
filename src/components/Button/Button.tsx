9b0413b644d1464f5f8f21c660e23f113f491157
Matias Vasquez <m.vasquezac@gmail.com>
2024-08-01 15:30:14 -0400

    feat: add button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,37 @@
+import cx from "classnames";
+import "./Button.css";
+import Spinner from "./Spinner";
+
+export enum ButtonType {
+  WHITE = "white",
+  RED = "red",
+}
+
+interface ButtonProps {
+  text?: string;
+  width?: number;
+  height?: number;
+  type: ButtonType;
+  isLoading?: boolean;
+  handleClick(): void;
+}
+
+export default function Button(props: ButtonProps) {
+  const { text, width, height, type, handleClick, isLoading = false } = props;
+
+  const buttonClassName = cx("tbk-button", {
+    white: type === ButtonType.WHITE,
+    red: type === ButtonType.RED,
+  });
+
+  return (
+    <button
+      className={buttonClassName}
+      style={{ width: width, height: height }}
+      onClick={handleClick}
+      disabled={isLoading}
+    >
+      {isLoading ? <Spinner /> : text}
+    </button>
+  );
+}
