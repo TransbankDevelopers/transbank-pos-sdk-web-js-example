da0120ae1bf5e1671a6862ca85197152e3292d69
Matias Vasquez <m.vasquezac@gmail.com>
2024-08-02 21:42:33 -0400

    feat: update app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import MainConnection from "./pages/MainConnection";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 className="text-4xl text-red-500">Hola</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-green-500">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <MainConnection />
+      <Footer />
     </div>
   );
 }
