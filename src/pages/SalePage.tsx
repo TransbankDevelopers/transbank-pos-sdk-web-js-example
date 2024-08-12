ff77b10257b29b53ba6da348a81b4c3da46ea0c5
Matias Vasquez <m.vasquezac@gmail.com>
2024-08-12 08:05:22 -0400

    feat: add page for sales

--- src/pages/SalePage.tsx
+++ src/pages/SalePage.tsx
@@ -1,7 +1,221 @@
-import React from "react";
+import ProductCard from "../components/ProductCard/ProductCard";
+import "./SalePage.css";
+import burgerImg from "../assets/burger.png";
+import friesImg from "../assets/french-fries.png";
+import iceCreamImg from "../assets/ice-cream.png";
+import coffeeImg from "../assets/coffee.png";
+import Button, { ButtonType } from "../components/Button/Button";
 
 const SalePage = () => {
-  return <div>SalePage</div>;
+  return (
+    <div className="sales-flex-container">
+      <div className="status-bar">
+        <p>
+          <span className="tbk-bold">Estado de conexión con agente: </span>
+          Conectado
+        </p>
+        <div className="flex">
+          <p className="border-r-gray5">
+            <span className="tbk-bold">Estado de punto de venta: </span>
+            Conectado al POS
+          </p>
+          <p>
+            <span className="tbk-bold">Estado de venta: </span>
+          </p>
+        </div>
+      </div>
+      <div className="pos-container">
+        <div className="product-container">
+          <ProductCard price={3990} imagePath={burgerImg}>
+            Hamburguesa
+          </ProductCard>
+          <ProductCard price={1990} imagePath={friesImg}>
+            Papas fritas
+          </ProductCard>
+          <ProductCard price={1750} imagePath={iceCreamImg}>
+            Helado
+          </ProductCard>
+          <ProductCard price={990} imagePath={coffeeImg}>
+            Café Latte
+          </ProductCard>
+        </div>
+        <div className="operations-container">
+          <div className="flex flex-col">
+            <div className="div1">
+              <p className="text-light bg-grey p-2">Operaciones</p>
+              <div className="button-container">
+                <Button
+                  width={148}
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Modo normal
+                </Button>
+                <Button
+                  width={148}
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Poll
+                </Button>
+                <Button
+                  width={148}
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Carga llaves
+                </Button>
+              </div>
+            </div>
+            <div className="div2 border-t-blue flex justify-between">
+              <div className="flex flex-col pos-field-container">
+                <span className="operation-title mb-4">Venta normal</span>
+                <input className="pos-input" type="text" placeholder="Monto" />
+                <Button
+                  additionalClass="mt-auto mb-7"
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Venta
+                </Button>
+              </div>
+              <div className="flex flex-col pos-field-container">
+                <span className="operation-title mb-4">Venta multicódigo</span>
+                <input className="pos-input" type="text" placeholder="Monto" />
+                <input
+                  className="pos-input mt-4 mb-4"
+                  type="text"
+                  placeholder="Cod. Comercio"
+                />
+                <Button
+                  additionalClass="mt-auto mb-7"
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Venta
+                </Button>
+              </div>
+              <div className="flex flex-col pos-field-container">
+                <span className="operation-title mb-4">Reembolso</span>
+                <input
+                  className="pos-input"
+                  type="text"
+                  placeholder="ID Operación"
+                />
+                <Button
+                  additionalClass="mt-auto mb-7"
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Rembolsar
+                </Button>
+              </div>
+            </div>
+            <div className="div3 border-t-blue flex justify-between">
+              <div className="flex flex-col pos-field-container">
+                <span className="operation-title mb-6">Última venta</span>
+                <span className="text-light">Obtener voucher:</span>
+                <div className="flex gap-8">
+                  <label className="tbk-radio-label">
+                    <input type="radio" name="option" value={1} />
+                    Si
+                  </label>
+                  <label className="tbk-radio-label">
+                    <input type="radio" name="option" value={0} />
+                    No
+                  </label>
+                </div>
+                <Button
+                  additionalClass="mt-auto"
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Última venta
+                </Button>
+              </div>
+              <div className="flex flex-col pos-field-container">
+                <span className="operation-title mb-6">Detalle venta</span>
+                <span className="text-light">Imprimir en POS:</span>
+                <div className="flex gap-8">
+                  <label className="tbk-radio-label">
+                    <input type="radio" name="option" value={1} />
+                    Si
+                  </label>
+                  <label className="tbk-radio-label">
+                    <input type="radio" name="option" value={0} />
+                    No
+                  </label>
+                </div>
+                <Button
+                  additionalClass="mt-auto"
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Detalle de venta
+                </Button>
+              </div>
+              <div className="flex flex-col pos-field-container">
+                <span className="operation-title mb-6">
+                  Detalle multicódigo
+                </span>
+                <span className="text-light">Imprimir en POS:</span>
+                <div className="flex gap-8">
+                  <label className="tbk-radio-label">
+                    <input type="radio" name="option" value={1} />
+                    Si
+                  </label>
+                  <label className="tbk-radio-label">
+                    <input type="radio" name="option" value={0} />
+                    No
+                  </label>
+                </div>
+                <Button
+                  additionalClass="mt-auto"
+                  height={40}
+                  type={ButtonType.POS}
+                  handleClick={() => {
+                    console.log("click");
+                  }}
+                >
+                  Detalle de venta
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="div4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas,
+            libero totam at quaerat maiores repellendus dolorem sapiente fugiat
+            debitis magnam non pariatur architecto officiis animi accusantium,
+            alias molestiae natus porro.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SalePage;
